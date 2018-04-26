11145e7802b582876bd3897f92b72c7bdbfc9bba
rajit88 <pavan.itsyou@gmail.com>
2018-04-26 11:26:08 +0530

    Update membersite_config.php

--- include/membersite_config.php
+++ include/membersite_config.php
@@ -4,20 +4,20 @@ require_once("./include/fg_membersite.php");
 $fgmembersite = new FGMembersite();
 
 //Provide your site name here
-$fgmembersite->SetWebsiteName('user11.com');
+$fgmembersite->SetWebsiteName('https://satyaregis-satyav1.azurewebsites.net/');
 
 //Provide the email address where you want to get notifications
-$fgmembersite->SetAdminEmail('user11@user11.com');
+$fgmembersite->SetAdminEmail('rajit88@gmail.com');
 
 //Provide your database login details here:
 //hostname, user name, password, database name and table name
 //note that the script will create the table (for example, fgusers in this case)
 //by itself on submitting register.php for the first time
 $fgmembersite->InitDB(/*hostname*/'localhost',
-                      /*username*/'prasanth',
-                      /*password*/'p',
-                      /*database name*/'testdb',
-                      /*table name*/'fgusers3');
+                      /*username*/'satyadbserver',
+                      /*password*/'Asdf12345678',
+                      /*database name*/'satyadb',
+                      /*table name*/'satyauser37');
 
 //For better security. Get a random string from this link: http://tinyurl.com/randstr
 // and put it here
